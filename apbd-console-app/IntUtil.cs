08dbcf334776e699df65ca10bc87908c12e9a54e
Simon Balcerzak <simonbalcerzak@gmail.com>
2025-03-09 15:59:44 +0100

    created class IntUtil with static method getAverage returning average from passed int array

---
+++ apbd-console-app/IntUtil.cs
@@ -0,0 +1,7 @@
+﻿namespace apbd_console_app;
+
+public class IntUtil {
+    public static double GetAverage(int[] intInputTable) {
+        return intInputTable.Average();
+    }
+}
--- apbd-console-app/Program.cs
+++ apbd-console-app/Program.cs
@@ -1,5 +1,7 @@
 ﻿// See https://aka.ms/new-console-template for more information
 
+using apbd_console_app;
+
 Console.WriteLine("Hello, World!");
 
 Console.WriteLine("Change number 1");
@@ -7,3 +9,6 @@ Console.WriteLine("Change number 1");
 Console.WriteLine("Change number 2");
 
 Console.WriteLine("Change number 3");
+
+
+Console.WriteLine(IntUtil.GetAverage([0, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10]));
